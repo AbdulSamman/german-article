2c2c68ee42fb0dfc1ca8174900366998de6cbf5e
AbdulSamman <sammanab@outlook.de>
2024-02-26 00:38:26 +0100

    article select works

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,8 @@ import { GermanNouns } from "./components/GermanNouns";
 function App() {
   return (
     <div>
-      <h1>Info Site</h1>
-      <p>Welcome th this site.</p>
-      <hr />
       <GermanNouns />
+      <hr />
     </div>
   );
 }
--- src/components/GermanNouns.tsx
+++ src/components/GermanNouns.tsx
@@ -4,8 +4,15 @@ import { useEffect, useState } from "react";
 const germanNounsUrl =
   "https://edwardtanguay.vercel.app/share/germanNouns.json";
 
+interface IGermanNoun {
+  article: string;
+  singular: string;
+  plural: string;
+}
+
 export const GermanNouns = () => {
   const [nouns, setNouns] = useState([]);
+  const [choice, setChoice] = useState("hide");
 
   useEffect(() => {
     (async () => {
@@ -15,10 +22,39 @@ export const GermanNouns = () => {
   }, []);
 
   return (
-    <div>
-      <h1>German Nouns</h1>
-      <p>Welcome to htis site.</p>
+    <div className="germanNouns">
+      <h1>German Article Practice</h1>
       <span>There are {nouns.length} nouns.</span>
+      <hr />
+      <div className="buttonsRow">
+        <button className="der" onClick={() => setChoice("der")}>
+          Der
+        </button>
+        <button className="die" onClick={() => setChoice("die")}>
+          Die
+        </button>
+        <button className="das" onClick={() => setChoice("das")}>
+          Das
+        </button>
+        <button className="hide" onClick={() => setChoice("hide")}>
+          Hide
+        </button>
+      </div>
+      <div className="nouns">
+        {nouns.map((noun: IGermanNoun, index) => {
+          return (
+            <div
+              key={index}
+              className="noun"
+              style={{
+                backgroundColor: noun.article == choice ? "green" : "#444",
+                color: noun.article == choice ? "#fff" : "#aaa",
+              }}>
+              {choice === noun.article && <>{noun.article}</>} {noun.singular}
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 };
